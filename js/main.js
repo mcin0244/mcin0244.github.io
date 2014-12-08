d52b53f707eae35c63441fb366b5d2ffe83c1a0a
mcin0244 <mcin0244@algonquinlive.com>
2014-12-08 04:33:24 -0500

    404 Page

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@ $(".work-grid a").on("click", function(eventobj){
 	$(id) .attr("data-state","open");
 	$(window).resize();
 })
+
+var $win = $(window);
+var $compass = $('.compass');
+var $main = $('.main');
+
+$win.on('scroll', function () {
+	var scrollPos = $win.scrollTop();
+
+	$compass.css('transform', 'rotate(' + scrollPos / 2 + 'deg)');
+	$main.css('background-position', 'center ' + scrollPos / 3 + 'px');
+});
